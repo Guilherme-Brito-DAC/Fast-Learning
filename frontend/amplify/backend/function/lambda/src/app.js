d426b09f8075b65c8b1405a9dea6b46af6dbdd88
Guilherme Brito de Almeida Costa <guilhermebritoalmeidacosta@gmail.com>
2021-11-03 09:51:03 -0300

    aws lambda

---
+++ frontend/amplify/backend/function/lambda/src/app.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+const app = express()
+app.use(cors())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+connection = mysql.createConnection({
+  host: "db4free.net",
+  user: "guigui",
+  password: "Aq'2xmK94s9#tdZ",
+  database: "objetos_aprendiz"
+})
+
+connection.connect()
+
+app.get('/', function (req, res) {
+
+  connection.query("SELECT * FROM modulo", function (error, results, fields) {
+      res.send(JSON.parse(JSON.stringify(results)))
+  })
+
+})
+
+app.get('/modulos/:id', function (req, res) {
+
+  var id = req.params.id
+  connection.query(`SELECT * FROM aula WHERE modulo_id = ${id}`, function (error, results, fields) {
+      res.send(JSON.parse(JSON.stringify(results)))
+  })
+
+})
+
+app.get('/aulas', function (req, res) {
+
+  connection.query(`SELECT * FROM aula`, function (error, results, fields) {
+      res.send(JSON.parse(JSON.stringify(results)))
+  })
+
+})
+
+app.get('/aulas/:id', function (req, res) {
+
+  var id = req.params.id
+  connection.query(`SELECT * FROM aula WHERE id = ${id}`, function (error, results, fields) {
+
+      if (results == undefined) {
+          results = {
+              id: 99,
+          }
+      }
+
+      res.send(JSON.parse(JSON.stringify(results)))
+  })
+
+})
+
+app.get('/search/:q', function (req, res) {
+
+  var q = req.params.q
+
+  connection.query(`SELECT * FROM aula WHERE titulo LIKE "%${q}%"`, function (error, results, fields) {
+
+      if (results === undefined || results.length === 0) {
+          results = {
+              id: 99,
+          }
+      }
+      else
+      {
+          results = results[0]
+      }
+
+      res.send(JSON.parse(JSON.stringify(results)))
+  })
+
+})
+
+app.listen(3000, () => {
+  console.log('Servidor aberto :)')
+})
+
+module.exports = app
